66de33fdba74ea1a2574bffe6abf3ac7da4da650
Vinayak <vinayakb807@gmail.com>
2021-10-10 00:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route("/recommandations", methods=["GET", "POST"])
 def fetch_recs():
     x = random.randint(1, 9999)
+    
     return render_template("index.html", Recommandation_text="Below are the recommendations", p1="a".format(x), p2="a2".format(x),p3="a".format(x), p4="a2".format(x),p5="a".format(x))
 
 
