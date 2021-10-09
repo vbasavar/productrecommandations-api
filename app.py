1cdaeed37926167d4c68209ecc669f988ea6912c
vinayak <vbasavaraddi@informatica.com>
2021-10-10 00:22:15 +0530

    Uploaded the files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+import os
+import random
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/recommandations", methods=["GET", "POST"])
+def fetch_recs():
+    return render_template("index.html", Recommandation_text="Below are the recommendations", p1="a", p2="a2",p3="a", p4="a2",p5="a")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
